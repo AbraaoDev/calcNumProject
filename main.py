5ce74a3cf1b001ce8a78fa8f091fc6b22bfde4b5
sammid37 <samanthadmedeiros37@gmail.com>
2022-04-21 12:04:38 -0300

    🧮 Arquivo principal

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-# Arquivo principal responsável pela Harmonização
-# ! Ainda é necessário testar a conexão
-
 import mysql.connector
 import matplotlib.pyplot as plt
 from numpy import log as ln
 
+# ------------------------------------- ETAPA 1
 # Calcula o IMC de cada jogador
 def calc_imc(m, h):
     imc = m / h**2
@@ -19,18 +17,40 @@ def calc_imc(m, h):
         return "IMC não saudavel"
 
 def calc_imc_medio(lista_imcs):
-    imc_medio = sum(lista_imcs) / len(lista_imcs)
-    return imc_medio
+    y = sum(lista_imcs) / len(lista_imcs)
+    return y
 
-# Cálculo da Taxa Metabólica Basal
+def delta_imc_medio(y):
+    delta_y = abs(25 - y) / y
+    return delta_y
+
+def imc_aceitavel(delta_y, y):
+    # onde y corresponde ao valor obtido na função imc_medio
+    k0 = (1-delta_y)*y 
+    k1 = (1+delta_y)*y
+
+    # verifica se o imc está entre o intervalo: k0 <= y <= k1
+    if(k0 <= y and y <= k1):
+        print("IMC aceitável")
+        return y
+    else:
+        print("Fora do intervalo")
+        return 0
+
+def calcular_altura_media(lista_alturas):
+    h = sum(lista_alturas) / len(lista_alturas)
+    return h
+
+# Calcula a h0 e h1 com relação ao IMC, ver anexo do professor
+def calc_h_min_h_max():
+    pass
+
+# ------------------------------------- ETAPA 2
+# Cálculo da Taxa Metabólica Basal e equação de Colebrook, ver anexo do professor
 def calc_tmb(idade, m, h):
     # equação proveniente do 'artigo 1' enviado pelo professor
     tmb = -0.1631 - 0.00255 * idade + 0.4721 * ln(m) + 0.2952 * ln(h)
     return tmb
-
-# Harmonização utilizando o método da equação de Cole, por não ser linear
-def harmonizar():
-    pass
 
 try:
     # tenta estabelecer a conexão com o bando de dados
